15a2d61d343a48bebc9e63ba1d0596efcc467b83
vladar21 <38045592+vladar21@users.noreply.github.com>
2021-01-28 17:15:17 +0200

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://appntmnt.heroku.com/';
+	public $baseURL = 'https://appntmnt.heroku.com/';
 
 	/*
 	|--------------------------------------------------------------------------
